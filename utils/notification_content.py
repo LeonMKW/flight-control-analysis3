4a83eb78f8f64d03894b75358e91cea25807fc54
wumingkun <wumingkun@yinhe.ht>
2024-05-17 14:54:49 +0800

    finalized odpa

--- utils/notification_content.py
+++ utils/notification_content.py
@@ -1,5 +1,6 @@
 import time
 from datetime import datetime
+import json
 
 
 def OBC_cumulative_reset_content(cumulative_reset_doc):
@@ -62,6 +63,10 @@ def od_precision_content(orbit_precision_summary, imgurl):
     # Get current timestamp (13 digits)
     current_timestamp = int(time.time() * 1000)
     epochTimeUTC = orbit_precision_summary['epochTimeUTC']
+    orbit_precision_summary_json = json.dumps(orbit_precision_summary)
+    # print(orbit_precision_summary_json)
+
+    ops = str(orbit_precision_summary_json).replace("{", "").replace("}", "")
 
     # Get current time in the desired format
     current_time_str = time.strftime("%Y-%m-%d %H:%M:%S")
@@ -70,34 +75,14 @@ def od_precision_content(orbit_precision_summary, imgurl):
     "code": "Satellite_Orbital_Accuracy_Update",
     "objectType": "satellite",
     "objectId": "1",
-    "objectName": "GS-2AP02",
+    "objectName": "{satellitecode}",
     "ruleName": "",
-    "eventTime": 1714982307298,
+    "eventTime": {current_timestamp},
     "params": {{
-        "a": 6888039.9320658,
-        "e": 0.0068392903074478,
-        "i": 63.440064231145,
-        "dw": 350.75765993486,
-        "xw": 344.99186253841,
-        "M": 333.43692458386,
-        "CD": 19.769662302768,
-        "remark": "无动力-自动定轨",
-        "gnssCount": 718,
-        "residual": 23.7182,
-        "type": "Gnss",
-        "epochTimeUTC": "2024-05-13T01:25:38.000Z",
-        "id": 25142,
-        "thrust": 0.0,
-        "isValid": 1,
-        "spacecraft": "GS-2AP02",
-        "timestamp": 1715563538,
-        "mse": 7.194813179582931,
-        "hour_error": 7.0384115390479565,
-        "max_error": 40.07423050515354,
-        "beijing_time": "2024-05-13 09:25:38",
+        {ops},
         "img": "{imgurl}"
-    }}
-}}'''
+        }}
+    }}'''
     return body
 
 
